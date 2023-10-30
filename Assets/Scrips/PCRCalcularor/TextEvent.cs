e100689e8a93fcf4e70e676cda195b082d6d5d47
1176321897@qq.com <>
2023-10-30 16:58:54 +0800

    修复ab不匹配

---
+++ Assets/Scrips/PCRCalcularor/TextEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace PCRCaculator.Guild
+{
+    public class TextEvent : Text
+    {
+        public event Action OnTextChanged;
+
+        public override string text
+        {
+            get => base.text;
+            set
+            {
+                base.text = value;
+                OnTextChanged?.Invoke();
+            }
+        }
+    }
+}
